4785b4b677e51e7ec9a8a5f512f541fdf5d81485
Geoo Chi <geoochi@geoochi.com>
2025-06-17 10:37:13 +0800

    update

--- remove-feed-xhs.js
+++ remove-feed-xhs.js
@@ -2,7 +2,7 @@
 // @name        小红书聚焦搜索
 // @homepage
 // @icon
-// @version     0.6
+// @version     0.7
 // @description 自动删除小红书页面上的navbar、推荐流以及其他信息，只留下搜索框
 // @author      geoochi
 // @license     MIT
@@ -12,17 +12,26 @@
 // ==/UserScript==
 
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
 ;(async function () {
-  await sleep(300);
+  await sleep(300)
   document.getElementById('mfContainer').remove()
-  
-  document.getElementsByClassName('mask-paper')[0].getElementsByClassName('active router-link-exact-active')[0].remove()
-  document.getElementsByClassName('mask-paper')[0].getElementsByClassName('right')[0].remove()
-  
+
+  document
+    .getElementsByClassName('mask-paper')[0]
+    .getElementsByClassName('active router-link-exact-active')[0]
+    .remove()
+  document
+    .getElementsByClassName('mask-paper')[0]
+    .getElementsByClassName('right')[0]
+    .remove()
+
   document.getElementsByClassName('side-bar')[0].remove()
 
-  document.getElementsByClassName('input-box')[0].style.cssText = 'position: absolute; top: 30vh;'
+  document.getElementsByClassName('bottom-menu')[0].remove()
+
+  document.getElementsByClassName('input-box')[0].style.cssText =
+    'position: absolute; top: 30vh;'
 })()
